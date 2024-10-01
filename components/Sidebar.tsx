813cdeafc7ceb6818dbdc0e5942d960126883a15
samfloyd9 <floyd.samuel.23@gmail.com>
2024-10-01 14:46:42 -0400

    fixed all deploy errors

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
 
   return (
-    <section className="sidebar">
+    <section className="sidebar z-50">
       <nav className="flex flex-col gap-4">
         <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
           <Image 
